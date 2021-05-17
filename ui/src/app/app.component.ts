80102f451a67a336d01688ad551b5a31bc9e149b
avhetman <andrei.hetman@hbo.com>
2021-05-17 15:34:52 -0600

    chore(ui): update demo page to use v0.4

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -19,7 +19,7 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
 
 export class AppComponent {
   scte35: SCTE35 = new SCTE35();
-  title = 'SCTE35-JS PARSER DEMO (v0.3.0)';
+  title = 'SCTE35-JS PARSER DEMO (v0.4.0)';
   scte35format = 'Base64';
   formats: string[] = ['Base64', 'Hexadecimal'];
   scte35FormControl = new FormControl('', [
